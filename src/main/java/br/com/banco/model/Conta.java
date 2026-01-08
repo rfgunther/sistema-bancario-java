979991578ff4035417430faa273df374ebbac209
rfgunther <rudolffg@gmail.com>
2026-01-08 14:10:46 -0300

    feat: classe Conta com número sequencial, agência fixa e toString básico

--- src/main/java/br/com/banco/model/Conta.java
+++ src/main/java/br/com/banco/model/Conta.java
@@ -1,6 +1,42 @@
+
+
 package br.com.banco.model;
 
-public class Cliente {
-    // Campos: gerencia saldo, número, agência, operações (depósito/saque/transferência) e lista de transações.
-    // Construtor, getters/setters e validações virão aqui
+public class Conta {
+    private int numero;
+    private static int proximoNumero = 1; // contador sequencial
+    private String agencia = "0001";
+    private double saldo = 0.0;
+    private Cliente titular;
+
+    public Conta(Cliente titular) {
+        this.titular = titular;
+        this.numero = proximoNumero++;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public String getAgencia() {
+        return agencia;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public Cliente getTitular() {
+        return titular;
+    }
+
+    @Override
+    public String toString() {
+        return "Conta{" +
+                "numero=" + numero +
+                ", agencia='" + agencia + '\'' +
+                ", saldo=" + saldo +
+                ", titular=" + titular.getNome() +
+                '}';
+    }
 }
